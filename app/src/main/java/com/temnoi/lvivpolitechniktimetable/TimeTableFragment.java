bf0e5a638b88e4e6a0eace9ed50a8064249af861
temnoi <chornenkyy@gmail.com>
2016-09-14 22:38:10 +0300

    move timetable fragment to the separate file

---
+++ app/src/main/java/com/temnoi/lvivpolitechniktimetable/TimeTableFragment.java
@@ -0,0 +1,41 @@
+package com.temnoi.lvivpolitechniktimetable;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TimeTableFragment extends Fragment {
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static TimeTableFragment newInstance(int sectionNumber) {
+        TimeTableFragment fragment = new TimeTableFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_time_table, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ((main) getActivity()).onSectionAttached(
+                getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+}
--- app/src/main/java/com/temnoi/lvivpolitechniktimetable/main.java
+++ app/src/main/java/com/temnoi/lvivpolitechniktimetable/main.java
@@ -96,7 +96,7 @@ public class main extends ActionBarActivity
         // update the my content by replacing fragments
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                .replace(R.id.container, TimeTableFragment.newInstance(position + 1))
                 .commit();
     }
 
@@ -212,42 +212,7 @@ public class main extends ActionBarActivity
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
 
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_my, container, false);
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((main) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
-    }
 
     class TimetableRefresh extends AsyncTask<Void, Integer, Void> {
         @Override

8202feb2fc2a47777f138e71fcc3f550904f1de3
Volodia Chornenkyy <volodia.chornenkyy@techmagic.co>
2016-09-15 10:04:23 +0300

    add newInstance() for groups fragment

--- app/src/main/java/com/temnoi/lvivpolitechniktimetable/ui/setup/GroupsFragment.java
+++ app/src/main/java/com/temnoi/lvivpolitechniktimetable/ui/setup/GroupsFragment.java
@@ -1,5 +1,6 @@
 package com.temnoi.lvivpolitechniktimetable.ui.setup;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
 /**
@@ -8,4 +9,16 @@ import android.support.v4.app.Fragment;
  */
 
 public class GroupsFragment extends Fragment {
+
+    private static final String ARG_UNIVERSITY_ID = "arg_university_id";
+
+    public static GroupsFragment newInstance(String universityId) {
+        Bundle bundle = new Bundle();
+        bundle.putString(ARG_UNIVERSITY_ID, universityId);
+
+        GroupsFragment groupsFragment = new GroupsFragment();
+        groupsFragment.setArguments(bundle);
+
+        return groupsFragment;
+    }
 }
